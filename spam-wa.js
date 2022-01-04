841b7a5f84874984ed841c5d747376a0314f03e9
Bagus TRIANURDIN <65612730+btrianurdin@users.noreply.github.com>
2022-01-04 19:22:27 +0700

    create spam-wa.js

---
+++ spam-wa.js
@@ -0,0 +1,23 @@
+var _run = null;
+
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+function runSpam(msg, interval) {
+  function spamFunction() {
+    const event = new Event('input', {
+      bubbles: true
+    });
+
+    let input = getElementByXpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div/div/div[2]");
+    input.innerHTML = msg;
+    input.dispatchEvent(event);
+
+    getElementByXpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button").click();
+  }
+
+  _run = setInterval(() => {
+    spamFunction();
+  }, interval);
+}
